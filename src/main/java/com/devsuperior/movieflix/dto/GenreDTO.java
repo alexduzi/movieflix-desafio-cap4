c153a281153cd67f5b9a79b7d031b45fc737171d
alexduzi <duzihd@gmail.com>
2025-05-01 20:24:51 -0300

    GenreController, GenreService, GenreRepository and integration tests

--- src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -1,9 +1,18 @@
 package com.devsuperior.movieflix.dto;
 
+import com.devsuperior.movieflix.entities.Genre;
+
 public class GenreDTO {
 
     private Long id;
     private String name;
+
+    public GenreDTO() {}
+
+    public GenreDTO(Genre genre) {
+        this.id = genre.getId();
+        this.name = genre.getName();
+    }
 
     public Long getId() {
         return id;
---
+++ src/main/java/com/devsuperior/movieflix/repositories/GenreRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.movieflix.repositories;
+
+import com.devsuperior.movieflix.entities.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
